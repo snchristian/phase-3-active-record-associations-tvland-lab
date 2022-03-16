12b4e0a211cf88a37ed01cefb608dc34597ec211
Shantel <snchristian8@gmail.com>
2022-03-16 12:22:38 -0400

    commit tree 4fbfc247db55794b1170402ce720f32407d21fc7

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    belongs_to :network
+
+    def actors_list
+        self.characters.map do |character|
+            character.actor.full_name
+        end
+    end
 end
